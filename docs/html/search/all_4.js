f9bd66714657b93d3b037f58751c5ab2a2fc513f
Dmitry Shishulin <70602663+ShishulinDmitry@users.noreply.github.com>
2024-10-19 17:58:47 +0300

    Delete docs/html directory

--- docs/html/search/all_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['save_5fkey_6',['save_key',['../main_8c.html#a45b28b22b985a6a4ce1d3ec3238810aa',1,'main.c']]],
-  ['sign_5ffile_7',['sign_file',['../main_8c.html#ae94ad925caebbb6ace99b37a8e768914',1,'main.c']]]
-];
--- docs/html/search/all_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['verify_5fsignature_8',['verify_signature',['../main_8c.html#a34e0197315b44a4239c52f2e737b7b9a',1,'main.c']]]
-];
